f98fac939ec3839c81c5ef9425721aa0ddaad98a
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-10-15 23:37:37 +0800

    Add files via upload

---
+++ leetcode_Review.py
@@ -0,0 +1,15 @@
+class solutionLeetcode_7:
+    def reverse(self, x: int) -> int:
+        s = str(x)[::-1].strip('-')
+        if int(s) < 2**31:
+            if x >= 0:
+                return int(s)
+            else:
+                return 0 - int(s)
+        return 0
+
+test_x = int(110)
+soln = solutionLeetcode_7
+reverse_x = soln.reverse(soln, test_x)
+
+print(reverse_x)
