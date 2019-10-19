a6856eb4f04c8ca5f5fc23f30f7c800ab1c24d26
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-10-19 23:53:52 +0800

    Update leetcode_Review.py

--- leetcode_Review.py
+++ leetcode_Review.py
@@ -8,6 +8,16 @@ class solutionLeetcode_7:
                 return 0 - int(s)
         return 0
 
+class solutionLeetcode_1:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for index, num in enumerate(nums):
+            another_num = target - num
+            if another_num in hashmap:
+                return [hashmap[another_num], index]
+            hashmap[num] = index
+        return None
+    
 test_x = int(110)
 soln = solutionLeetcode_7()
 reverse_x = soln.reverse(soln, test_x)
