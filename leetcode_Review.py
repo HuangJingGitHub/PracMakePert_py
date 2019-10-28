c5fc956d9c01553487fb0fe015826d216bdf19b4
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-10-28 23:47:07 +0800

    Add files via upload

--- leetcode_Review.py
+++ leetcode_Review.py
@@ -30,6 +30,22 @@ class solutionLeetcode_4:
         else:
             return nums1[(len(nums1)-1)//2]
 
+
+class solutionLeetcode_5:
+    def longestPalindrome(self, s):
+        str_length = len(s)
+        max_length = 0
+        start = 0
+        for i in range(str_length):
+            if i - max_length >= 1 and s[i - max_length - 1:i + 2] == s[i - max_length - 1:i+2][::-1]:
+                start = i - max_length - 1
+                max_length += 2
+                continue
+            if i - max_length >= 0 and s[i - max_length:i + 2] == s[i - max_length:i + 2][::-1]:
+                start = i - max_length
+                max_length += 1
+        return s[start:start + max_length + 1]
+
 class solutionLeetcode_7:
     def reverse(self, x) -> int:
         s = str(x)[::-1].strip('-')
