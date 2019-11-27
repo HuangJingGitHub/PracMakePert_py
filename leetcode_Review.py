76e5a3d784244d5674d8488e273816ea6d04df32
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-11-27 23:43:35 +0800

    Update leetcode_Review.py

--- leetcode_Review.py
+++ leetcode_Review.py
@@ -127,7 +127,7 @@ class solutionLeetcode_10:
         if not pattern:
             return not text
 
-        first = bool(text) and pattern[0] in {text[0], '.'}
+        first = bool(text) and (pattern[0] in {text[0], '.'})
 
         if len(pattern) >= 2 and pattern[1] == '*':
             return self.isMatch(text, pattern[2:]) or (first and self.isMatch(text[1:], pattern))
