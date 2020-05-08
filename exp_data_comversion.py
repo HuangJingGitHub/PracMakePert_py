e36c3c1f82f8286296f1a6d5d2c66b4094bf4343
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-05-08 15:03:39 +0800

    Add files via upload

--- exp_data_comversion.py
+++ exp_data_comversion.py
@@ -3,14 +3,14 @@ import scipy.io
 from os import listdir
 from os.path import isfile, join
 
-data_path = 'D:/Files/Research/Manipulation/Nonfixed Contact/Experiments/data/0501/'
-mat_save_path = 'D:/Files/Research/Manipulation/Nonfixed Contact/Experiments/data/0501/mat/'
+data_path = 'D:/Files/Research/Manipulation/Nonfixed Contact/Experiments/data/0506/'
+mat_save_path = 'D:/Files/Research/Manipulation/Nonfixed Contact/Experiments/data/0506/mat/'
 data_file_list = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 print(data_file_list)
 
 for data_file in data_file_list:
     data_file_path = data_path + data_file
-    postfix = data_file[data_file.rfind('_') : data_file.rfind('.')]
+    postfix = data_file[data_file.find('_') : data_file.rfind('.')]
 
     data_np = np.load(data_file_path)
-    scipy.io.savemat(mat_save_path + 'pt' + postfix + '.mat', {'pt' + postfix: data_np})
+    scipy.io.savemat(mat_save_path + 'angle' + postfix + '.mat', {'angle' + postfix: data_np})
