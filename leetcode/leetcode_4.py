37692e140c866eb61fb5e03d08852c3f5c7a1ca0
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-02-11 00:08:48 +0800

    Update leetcode_4.py

--- leetcode/leetcode_4.py
+++ leetcode/leetcode_4.py
@@ -35,3 +35,56 @@ class Solution:
             return (temp[0] + temp[1]) / 2
         else:
             return temp[1]
+
+# log(len(nums1) + len(nums2)) time        
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        totalLen = len(nums1) + len(nums2)
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+        if len(nums1) == 0:
+            if len(nums2) % 2 != 0:
+                return nums2[len(nums2) // 2]
+            else:
+                return (nums2[len(nums2) // 2] + nums2[len(nums2) // 2 - 1]) / 2
+            
+        lEdge = 0 
+        rEdge = len(nums1)
+        curA = 0
+        curB = 0
+        res = 0.0
+
+        while lEdge <= rEdge:
+            curA = lEdge + (rEdge - lEdge) // 2
+            curB = (totalLen + 1) // 2 - curA
+            if curA == 0:
+                LA = -100000000
+            else:
+                LA = nums1[curA - 1]
+            if curA == len(nums1):
+                RA = 100000000
+            else:
+                RA = nums1[curA]
+
+            if curB == 0:
+                LB = -100000000
+            else:
+                LB = nums2[curB - 1]
+            if curB == len(nums2):
+                RB = 100000000
+            else:
+                RB = nums2[curB]
+
+            if LA > RB:
+                rEdge = curA - 1
+            elif LB > RA:
+                lEdge = curA + 1
+            else:
+                if totalLen % 2 != 0:
+                    result = max(LA, LB)
+                else:
+                    result = (max(LA, LB) + min(RA, RB)) / 2.0
+                break
+        return result           
+                
+        
