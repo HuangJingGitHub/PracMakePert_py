8b2929c966446b2a314d92fa64b4e056f962475e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-05-11 23:47:44 +0800

    Create leetcode_51.py

---
+++ leetcode/leetcode_51.py
@@ -0,0 +1,46 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [['.' for i in range(n)] for j in range(n)]
+        self.backtrace(board, 0, res)
+        return res
+    
+    def backtrace(self, board: List[List[str]], row: int, res: List[str]) -> None:
+        n = len(board)
+        if row == n:
+            curRes = []
+            for boardRow in board:
+                curRes.append("".join(boardRow))
+            res.append(curRes.copy())
+            print(res)
+            return
+        
+        for col in range(n):
+            if not self.isValid(board, row, col):
+                continue
+            board[row][col] = 'Q'
+            self.backtrace(board, row + 1, res)
+            board[row][col] = '.'
+    
+    def isValid(self, board: List[List[str]], row: int, col: int) -> bool:
+        for i in range(row):
+            if board[i][col] == 'Q':
+                return False
+
+        n = len(board)
+        i = row - 1
+        j = col + 1
+        while i >= 0 and j < n:
+            if board[i][j] == 'Q':
+                return False
+            i -= 1
+            j += 1
+        
+        i = row - 1
+        j = col - 1
+        while i >= 0 and j >= 0:
+            if board[i][j] == 'Q':
+                return False
+            i -= 1
+            j -= 1
+        return True
