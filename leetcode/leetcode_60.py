25fefbdb4de7efaa5e5e512c53dd2cd623d33ddf
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-06-05 17:04:35 +0800

    Update leetcode_60.py

--- leetcode/leetcode_60.py
+++ leetcode/leetcode_60.py
@@ -14,3 +14,11 @@ class Solution:
             k -= interval * math.factorial(n)
         
         return res
+
+            # interval = (k - 1) // n! instead of k // n! in order to deal with cases where k % n! == 0, 
+            # in these cases, interval index is k // n! - 1. If k cannot be divided by n!, interval = k // n! = (k - 1) // n!
+            # so following code can also be used
+            # if k % math.factorial(n) == 0:
+            #     interval = k // math.factorial(n) - 1
+            # else:
+            #     interval = k // math.factorial(n)
