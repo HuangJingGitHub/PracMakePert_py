b8c663a6afb988e451e865a09cde70ba52c6a9cf
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-09-25 23:18:48 +0800

    Create leetcode_76.py

---
+++ leetcode/leetcode_76.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tStrLog = {}
+        for c in t:
+            if c not in tStrLog:
+                tStrLog[c] = 1
+            else:
+                tStrLog[c] += 1
+
+
+        charLog = {}
+        for k, v in tStrLog.items():
+            charLog[k] = [0 for i in range(0, len(s) + 1)]
+
+        if s[0] in charLog:
+            charLog[s[0]][1] = 1
+        for i in range(1, len(s)):
+            for k, v in charLog.items():
+                charLog[k][i + 1] = charLog[k][i]
+            if s[i] in charLog:
+                charLog[s[i]][i + 1] += 1
+        
+        left = 0
+        right = 0
+        resLen = 1e10
+        resLeft = 0
+        
+        print(charLog)
+        while right < len(s):
+            cover = True
+            for k, v in tStrLog.items():
+                if charLog[k][right + 1] - charLog[k][left] < v:
+                    cover = False
+                    break
+
+            if cover:
+                resLen = min(resLen, right - left + 1)
+                resLeft = left
+                left += 1
+            else:
+                right += 1
+
+        if resLen == 1e10:
+            return ''
+        return s[resLeft : resLeft + resLen + 1]
+        
