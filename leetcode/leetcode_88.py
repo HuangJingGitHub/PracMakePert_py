b474acafb85a643060220cb5c68877ce381cbc35
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-10-13 23:32:23 +0800

    Update leetcode_88.py

--- leetcode/leetcode_88.py
+++ leetcode/leetcode_88.py
@@ -3,11 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        pt1 = 0
-        pt2 = 0
+        if m == 0:
+            for i in range(n):
+                nums1[i] = nums2[i]
+            return
+        if n == 0:
+            return
+        
+        resIdx = m + n - 1
+        pt1 = m - 1
+        pt2 = n - 1
 
-        for i in range(m + n):
-            if nums1[pt1] <= nums2[pt2]:
-                pt1 += 1
+        while resIdx >= 0:
+            if nums1[pt1] >= nums2[pt2]:
+                nums1[resIdx] = nums1[pt1]
+                pt1 -= 1
             else:
-                for j in range(pt)
+                nums1[resIdx] = nums2[pt2]
+                pt2 -= 1
+            resIdx -= 1
+            if pt1 == -1 or pt2 == -1:
+                break
+
+        if pt1 == -1:
+            for j in range(pt2, -1, -1):
+                nums1[resIdx] = nums2[j]
+                resIdx -= 1
+        
