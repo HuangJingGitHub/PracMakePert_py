2c120e1d49b4d0afd1c6aa637c66febacf2b616b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-11-17 15:39:49 -0800

    Update nn_classifier.py

--- pytorch_course/nn_classifier.py
+++ pytorch_course/nn_classifier.py
@@ -35,102 +35,103 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-batch_size = 4
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-classes = ('plane', 'car', 'bird', 'cat',
-        'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+if __name__ == '__main__':
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    batch_size = 4
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-net = Net()
-need_train = True
-PATH = './cifar_net.pth'
-if need_train:
-    print('Start CPU training')
-    start_time = time.time()
-if need_train:
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(2):
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss =criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+    net = Net()
+    need_train = True
+    PATH = './cifar_net.pth'
+    if need_train:
+        print('Start CPU training')
+        start_time = time.time()
+    if need_train:
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        for epoch in range(5):
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                loss =criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-            running_loss += loss.item()
-            if i % 2000 == 1999:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss /2000:.3f}')
+                running_loss += loss.item()
+                if i % 2000 == 1999:
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss /2000:.3f}')
+                    running_loss = 0.0
+        print('Finish CPU training in %s s' %(time.time() - start_time))
+        torch.save(net.state_dict(), PATH)
+
+    net.load_state_dict(torch.load(PATH, weights_only=True))
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+    # imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}' for j in range(4)))
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total}%')
+
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            for label, predict in zip(labels, predicted):
+                if label == predict:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+
+    # Training on GPU
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    if device == 'cuda:0':
+        net_gpu = Net()
+        net_gpu.to(device)
+        if need_train:
+            print('Start GPU training')
+            start_time = time.time()
+        if need_train:
+            criterion = nn.CrossEntropyLoss()
+            optimizer = optim.SGD(net_gpu.parameters(), lr=0.001, momentum=0.9)
+            for epoch in range(2):
                 running_loss = 0.0
-    print('Finish CPU training in %s s' %(time.time() - start_time))
-    torch.save(net.state_dict(), PATH)
+                for i, data in enumerate(trainloader, 0):
+                    inputs, labels = data[0].to(device), data[1].to(device)
+                    optimizer.zero_grad()
+                    outputs = net_gpu(inputs)
+                    loss =criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
 
-net.load_state_dict(torch.load(PATH, weights_only=True))
-dataiter = iter(testloader)
-images, labels = next(dataiter)
-# imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
-
-outputs = net(images)
-_, predicted = torch.max(outputs, 1)
-print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}' for j in range(4)))
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total}%')
-
-correct_pred = {classname: 0 for classname in classes}
-total_pred = {classname: 0 for classname in classes}
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        for label, predict in zip(labels, predicted):
-            if label == predict:
-                correct_pred[classes[label]] += 1
-            total_pred[classes[label]] += 1
-
-for classname, correct_count in correct_pred.items():
-    accuracy = 100 * float(correct_count) / total_pred[classname]
-    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
-
-# Training on GPU
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-net_gpu = Net()
-net_gpu.to(device)
-if need_train:
-    print('Start GPU training')
-    start_time = time.time()
-if need_train:
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net_gpu.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(2):
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data[0].to(device), data[1].to(device)
-            optimizer.zero_grad()
-            outputs = net_gpu(inputs)
-            loss =criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-            if i % 2000 == 1999:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss /2000:.3f}')
-                running_loss = 0.0
-    print('Finish GPU training in %s s' %(time.time() - start_time))
+                    running_loss += loss.item()
+                    if i % 2000 == 1999:
+                        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss /2000:.3f}')
+                        running_loss = 0.0
+            print('Finish GPU training in %s s' %(time.time() - start_time))
