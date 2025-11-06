a2cd17c45532f02d0d838bab590459353042d960
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-11-06 20:55:22 +0800

    Update 3d_delaunay_scipy.py

--- 3d_delaunay_scipy.py
+++ 3d_delaunay_scipy.py
@@ -40,3 +40,53 @@ fig = plt.figure()
 ax = plt.axes(projection='3d')
 plot_tri_simple(ax, dela)
 plt.show()
+
+class SAMFeedback:
+    def __init__(self):
+        from sam2.build_sam import build_sam2_camera_predictor
+
+        self.sam2_checkpoint = "/root/sam/segment-anything-2-real-time/checkpoints/sam2.1_hiera_small.pt"
+        self.sam2_model_cfg = "configs/sam2.1/sam2.1_hiera_s.yaml" # use relative path to sam package
+        self.predictor = build_sam2_camera_predictor(self.sam2_model_cfg, self.sam2_checkpoint)
+        self.initialized = False
+
+    def init_with_points(self, frame, prompt_points, prompt_labels):
+        self.predictor.load_first_frame(frame)
+        _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
+            0,
+            0,
+            points=np.array(prompt_points, dtype=np.float32),
+            labels=np.array(prompt_labels, dtype=np.int32),
+        )
+        self.initialized = True
+    
+    def init_with_center_pts(self, frame, center_point, bbox_length=30):
+        self.predictor.load_first_frame(frame)
+        np_pt = np.array(center_point, dtype=np.float32)
+        bbox = np.array([np_pt - bbox_length, np_pt + bbox_length], dtype=np.float32)
+        _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
+            0,
+            0,
+            bbox=bbox
+        )
+        self.initialized = True
+    
+    def init_with_bbox(self, frame, bbox):
+        self.predictor.load_first_frame(frame)
+        _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
+            0,
+            0,
+            bbox=bbox
+        )
+        self.initialized = True
+
+    def track(self, frame):
+        if not self.initialized:
+            raise RuntimeError("SAMFeedback not initialized.")
+        self.out_obj_ids, self.out_mask_logits = self.predictor.track(frame)
+        self.mask = (self.out_mask_logits[0][0] > 0).cpu().numpy().astype("uint8")
+        
+        # color = np.array([100, 100, 100])
+        # h, w = self.mask.shape[-2:]
+        # mask_image = self.mask.reshape(h, w, 1) * color.reshape(1, 1, -1).astype("uint8")
+        return self.mask
