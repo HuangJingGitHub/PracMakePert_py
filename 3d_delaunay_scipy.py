db57702ca2610b580a03e3217a3b84d839407c06
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-12-12 20:17:41 +0800

    Add files via upload

--- 3d_delaunay_scipy.py
+++ 3d_delaunay_scipy.py
@@ -1,92 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-from scipy.spatial import Delaunay as delaa
+from scipy.spatial import Delaunay
 
-pts=np.array([[ 0.,  0.,  0.],
-       [ 0.,  0.,  8.],
-       [ 0.,  8.,  0.],
-       [ 0.,  8.,  8.],
-       [ 0., 16.,  0.],
-       [ 0., 16.,  8.],
-       [ 8.,  0.,  0.],
-       [ 8.,  0.,  8.],
-       [ 8.,  8.,  0.],
-       [ 8.,  8.,  8.],
-       [ 8., 16.,  0.],
-       [ 8., 16.,  8.],
-       [16.,  0.,  0.],
-       [16.,  0.,  8.],
-       [16.,  8.,  0.],
-       [16.,  8.,  8.],
-       [16., 16.,  0.],
-       [16., 16.,  8.]])
-	   
-dela=delaa(pts)
+pt_num = 5
+pts = np.random.rand(pt_num, 3)
+# env_vertices = np.array([
+#     [0.0, 0.0, 0.0] + np.random.rand(3) * 0.01,
+#     [1.0, 0.0, 0.0] + np.random.rand(3) * 0.01,
+#     [1.0, 1.0, 0.0] + np.random.rand(3) * 0.01,
+#     [0.0, 1.0, 0.0] + np.random.rand(3) * 0.01,
+#     [0.0, 0.0, 1.0] + np.random.rand(3) * 0.01,
+#     [1.0, 0.0, 1.0] + np.random.rand(3) * 0.01,
+#     [1.0, 1.0, 1.0] + np.random.rand(3) * 0.01,
+#     [0.0, 1.0, 1.0]+ np.random.rand(3) * 0.01,
+# ])
+# pts = np.append(pts, env_vertices, axis=0)
+pts[:, 0] *= 100
+pts[:, 1] *= 200
+pts[:, 2] *= 100
+dela = Delaunay(pts)
+print(f"delaunay meshes:\n{dela.__dict__}")
 
 def plot_tri_simple(ax, dela):
     for tr in dela.simplices:
         pts = dela.points[tr, :]
-        ax.plot3D(pts[[0,1],0], pts[[0,1],1], pts[[0,1],2], color='g', lw='0.1')
-        ax.plot3D(pts[[0,2],0], pts[[0,2],1], pts[[0,2],2], color='g', lw='0.1')
-        ax.plot3D(pts[[0,3],0], pts[[0,3],1], pts[[0,3],2], color='g', lw='0.1')
-        ax.plot3D(pts[[1,2],0], pts[[1,2],1], pts[[1,2],2], color='g', lw='0.1')
-        ax.plot3D(pts[[1,3],0], pts[[1,3],1], pts[[1,3],2], color='g', lw='0.1')
-        ax.plot3D(pts[[2,3],0], pts[[2,3],1], pts[[2,3],2], color='g', lw='0.1')
+        ax.plot3D(pts[[0, 1], 0], pts[[0, 1], 1], pts[[0, 1], 2], color="g", lw="0.5")
+        ax.plot3D(pts[[0, 2], 0], pts[[0, 2], 1], pts[[0, 2], 2], color="g", lw="0.5")
+        ax.plot3D(pts[[0, 3], 0], pts[[0, 3], 1], pts[[0, 3], 2], color="g", lw="0.5")
+        ax.plot3D(pts[[1, 2], 0], pts[[1, 2], 1], pts[[1, 2], 2], color="g", lw="0.5")
+        ax.plot3D(pts[[1, 3], 0], pts[[1, 3], 1], pts[[1, 3], 2], color="g", lw="0.5")
+        ax.plot3D(pts[[2, 3], 0], pts[[2, 3], 1], pts[[2, 3], 2], color="g", lw="0.5")
 
-    ax.scatter(dela.points[:,0], dela.points[:,1], dela.points[:,2], color='b')
+    ax.scatter(dela.points[:, 0], dela.points[:, 1], dela.points[:, 2], color="b")
+    for i, p in enumerate(dela.points):
+        ax.text(p[0], p[1], p[2], f"{i}", color="r")
+
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax = plt.axes(projection="3d")
 plot_tri_simple(ax, dela)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+ax.set_aspect("equal")
 plt.show()
-
-class SAMFeedback:
-    def __init__(self):
-        from sam2.build_sam import build_sam2_camera_predictor
-
-        self.sam2_checkpoint = "/root/sam/segment-anything-2-real-time/checkpoints/sam2.1_hiera_small.pt"
-        self.sam2_model_cfg = "configs/sam2.1/sam2.1_hiera_s.yaml" # use relative path to sam package
-        self.predictor = build_sam2_camera_predictor(self.sam2_model_cfg, self.sam2_checkpoint)
-        self.initialized = False
-
-    def init_with_points(self, frame, prompt_points, prompt_labels):
-        self.predictor.load_first_frame(frame)
-        _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
-            0,
-            0,
-            points=np.array(prompt_points, dtype=np.float32),
-            labels=np.array(prompt_labels, dtype=np.int32),
-        )
-        self.initialized = True
-    
-    def init_with_center_pts(self, frame, center_point, bbox_length=30):
-        self.predictor.load_first_frame(frame)
-        np_pt = np.array(center_point, dtype=np.float32)
-        bbox = np.array([np_pt - bbox_length, np_pt + bbox_length], dtype=np.float32)
-        _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
-            0,
-            0,
-            bbox=bbox
-        )
-        self.initialized = True
-    
-    def init_with_bbox(self, frame, bbox):
-        self.predictor.load_first_frame(frame)
-        _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
-            0,
-            0,
-            bbox=bbox
-        )
-        self.initialized = True
-
-    def track(self, frame):
-        if not self.initialized:
-            raise RuntimeError("SAMFeedback not initialized.")
-        self.out_obj_ids, self.out_mask_logits = self.predictor.track(frame)
-        self.mask = (self.out_mask_logits[0][0] > 0).cpu().numpy().astype("uint8")
-        
-        # color = np.array([100, 100, 100])
-        # h, w = self.mask.shape[-2:]
-        # mask_image = self.mask.reshape(h, w, 1) * color.reshape(1, 1, -1).astype("uint8")
-        return self.mask
